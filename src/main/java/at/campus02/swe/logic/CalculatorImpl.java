234ab7557a08c90846b6e16dbc6e12d0fc6787a4
wolfi <wofoeif@gaoie.com>
2023-12-16 13:47:15 +0100

    Calculator um Modulo erweitert
    closes #4

--- src/main/java/at/campus02/swe/logic/CalculatorImpl.java
+++ src/main/java/at/campus02/swe/logic/CalculatorImpl.java
@@ -28,6 +28,8 @@ public class CalculatorImpl implements Calculator {
                 return c;
             case mul:
                 return a * b;
+            case mod:
+                return a % b;
         }
         return 0;
     }
